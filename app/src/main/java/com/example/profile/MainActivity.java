db7ba271ae71576c24eefbce17ae27c50547c43f
Anannya <aaniee09@gmail.com>
2020-10-11 16:45:06 +0530

    '

--- app/src/main/java/com/example/profile/MainActivity.java
+++ app/src/main/java/com/example/profile/MainActivity.java
@@ -4,17 +4,29 @@ package com.example.profile;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button submitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
+            submitButton = findViewById(R.id.submitProfile);
+            submitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openSubmit();
+                }
+            });
 
             ActionBar actionBar = getSupportActionBar();
             if (actionBar != null) {
@@ -22,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        public boolean onOptionsItemSelected(MenuItem item){
+    private void openSubmit() {
+        Toast.makeText(this, "Your profile is updated.", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
             switch (item.getItemId()) {
                 case android.R.id.home:
                     finish();
